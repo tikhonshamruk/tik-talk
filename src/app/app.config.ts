352c517a0d784e5b451690e25dba268845208ae4
tikhon <tikhonshamruk@gmail.com>
2025-02-12 17:13:06 +0300

    add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loggingInterceptorFunctional } from './auth/auth.interceptor';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     provideHttpClient(
-      withInterceptors([loggingInterceptorFunctional])
+      withInterceptors([loggingInterceptorFunctional]),
     ),
+    {provide:LocationStrategy,useClass:HashLocationStrategy}
   ]
 
 };
